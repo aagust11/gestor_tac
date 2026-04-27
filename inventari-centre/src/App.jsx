43a5126e4581c81526f2be608ba96227994209d6
aagust11 <132393883+aagust11@users.noreply.github.com>
2026-04-27 19:10:41 +0200

    Scaffold inventari-centre Vite React project

---
+++ inventari-centre/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="app-shell">
+      <section className="card">
+        <h1>Inventari Centre</h1>
+        <p>Projecte inicial preparat amb Vite + React i HashRouter.</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
